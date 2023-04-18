b2f3272b963a78de21f24aeb5f36cf59f9844525
Anup kapale <anup.kapale@gmail.com>
2023-04-18 20:20:46 +0530

    json component has been added

--- src/test/java/com/uiframework/tests/TestLaunch.java
+++ src/test/java/com/uiframework/tests/TestLaunch.java
@@ -2,16 +2,21 @@ package com.uiframework.tests;
 
 import com.uiframework.base.BaseTest;
 import com.uiframework.objects.BillingAddress;
+import com.uiframework.objects.Product;
 import com.uiframework.pages.*;
+import com.uiframework.utils.JacksonUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class TestLaunch extends BaseTest {
 
 
 
     @Test
-    public void guestCheckoutUsingDirectBankTransfer() throws InterruptedException {
+    public void guestCheckoutUsingDirectBankTransfer() throws InterruptedException, IOException {
 
 /*
         BillingAddress billingAddress = new BillingAddress ()
@@ -23,19 +28,24 @@ public class TestLaunch extends BaseTest {
                 .setEmail ( "asktest@gmail.com" );
 */
 
-        BillingAddress billingAddress = new BillingAddress ("demo", "user","San Fransisco","San Fransisco",
-                "94188","asktest@gmail.com");
+        /*BillingAddress billingAddress = new BillingAddress ("demo", "user","San Fransisco","San Fransisco",
+                "94188","asktest@gmail.com");*/
+
+        //parse jason object to java object
+       BillingAddress billingAddress = JacksonUtils.deserializeJson ( "BillingAddress.json", BillingAddress.class );
+        Product product = new Product ( 1211 );
+
 
         StorePage storePage = new HomePage ( driver )
                 .launchURL ()
                 .navigateToStoreMenuLink ()
-                .searchProduct ( "Blue" );
-        Assert.assertEquals ( storePage.getTitle (),"Search results: “Blue”");
+                .searchProduct ( "dark" );
+        Assert.assertEquals ( storePage.getTitle (),"Search results: “dark”");
 
-        storePage.clickAddToCart ("Blue Shoes");
+        storePage.clickAddToCart (product.getName ());
         Thread.sleep ( 5000 );
         CartPage cartPage = storePage.clickViewCart ();
-        Assert.assertEquals (cartPage.getPrductName (),"Blue Shoes");
+        Assert.assertEquals (cartPage.getPrductName (),product.getName ());
 
         CheckOutPage checkOutPage = cartPage.
                 checkout ()
